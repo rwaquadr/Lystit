6988784091c9b8017c3ef638af41d677134a7b33
andresn93 <andresn93@gmail.com>
2017-07-12 10:14:48 -0500

    first / ajax post / mongo post

--- lystit-api/server.js
+++ lystit-api/server.js
@@ -2,55 +2,34 @@ const express = require('express')
 const app = express()  
 const port = 3000
 var bodyParser = require('body-parser')
-var fs = require('fs');
+// var fs = require('fs');
 var mongoose = require('mongoose')
 
-Schema = new mongoose.Schema({
-	id : String,
-	Description: String,
-	Schedule_Date: Boolean
-}),
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
 
-Todo = mongoose.model('Todo', Schema);
+//Check if server.js is running
+app.listen(port, function(err) {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`Magic is happening on ${port}`)
+});
 
+//Connect to Mongo
 mongoose.connect('mongodb://heroku_qgcjr9k0:dk9hse1csiu0l52rljicap4vsc@ds153392.mlab.com:53392/heroku_qgcjr9k0', function(error){
 	if (error) console.error(error);
 	else console.log('mongo connected');
 });
 
-	var obj;
-	fs.readFile('data.json', 'utf8', function (err, data){
-		if(err){
-			throw err
-		}
-		for(var i = 0; i < allTodos.length; i++){
-			// var obj = JSON.stringify(data.json);
+//MongoDB schema
+Schema = new mongoose.Schema({
+	description: String,
+	due_date: String
+    },{ collection: 'todo' });
 
-			console.log(obj);
-		}
-
-		// allTodos = JSON.parse(obj);
-		// console.log(data);
-	});
-	// console.log(obj);
-	
-
-
- 
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-app.listen(port, function(err) {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
- console.log(todos);
-
-  console.log(`Magic is happening on ${port}`)
-});
-
+var Todo = mongoose.model('todo', Schema);
 
 
 app.get('/todo-app', function(request, response) {  
@@ -59,42 +38,47 @@ app.get('/todo-app', function(request, response) {
 });
 
 
-var allTodos = [{
-	"description" : "feed the dog",
-	"due_date" : "7/5/17",
-	"ID" : 1,
-},
+//GET Todos from MongoDB 
 
-{
-	"description" : "wash the dishes",
-	"due_date" : "7/6/17",
-	"ID" : 2,
-},
-
-{
-	"description" : "take out the trash",
-	"due_date" : "7/7/17",
-	"ID" : 3,
-},]
-
-
- 	
-
-
-app.get('/get-allTodos', function(request, response){
-	response.send(allTodos);
+app.get('/get-allTodos', function(request, response){ 
+	response.send(Todo);
 	console.log('this route is being hit',allTodos);
 });
 
-app.post('/post-newTodos', function(request, response){
-	
-	
-	var newTodo = request.body;
-	allTodos.push(newTodo);
-	console.log(allTodos);
+app.get('/get-allTodos', function(request, response){
+	response.send(Todo);
 
+	Todo.find({}, function(err, todo){
+		console.log('we are here');
+		if(err){
+			console.log('ERROR:',err)
+		}else{
+			response.send(todo);
+		}
+	});
+
+});
+
+
+//POST newTodos to MongoDB
+app.post('/post-newTodos', function(request, response){
+	console.log(request.body);
+	var todo = new Todo(request.body);
+		console.log("here!",todo);
+	
+	todo.save(function (err,todo){
+		if(err){
+			console.log(err);
+		}else{
+			// console.log(todo);
+			// response.send(todo);
+			response.json(todo);
+		}
+	});
 });	
 
+
+//EDIT
  app.post('/post-edit', function(request, response){
  	
  	var requestData = request.body;
@@ -111,10 +95,4 @@ app.post('/post-newTodos', function(request, response){
  		}
  	}
 
-
  });
-
-
-
-	// response.send(request.body);
-	// console.log('this route is being hit');
